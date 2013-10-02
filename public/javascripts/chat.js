1d44ccbbd0af3203d3b780adfb5b76bb908a5afa
Dan Carlstedt <dancarlstedt@gmail.com>
2013-10-01 21:13:18 -0500

    Fixing client side bug in Chat.js

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -29,7 +29,7 @@ Chat.prototype.processCommand = function(command){
 			var room = words.join(' ');
 			this.changeRoom(room);
 			break;
-		case "nick"
+		case "nick":
 			words.shift();
 			var name = words.join(' ');
 			this.socket.emit('nameAttempt', name);
@@ -40,3 +40,57 @@ Chat.prototype.processCommand = function(command){
 	}
 	return message;
 }
+
+var socket = io.connect();
+$(document).ready(function(){
+	var chatApp = new Chat(socket);
+
+	socket.on('nameResult', function(result){
+		var message;
+
+		if(result.success){
+			message = 'You are now known as ' + result.name + '.';
+		} else {
+			message = result.message;
+		}
+
+		 $('#messages').append(divSystemConentElement(message));
+	});
+
+	socket.on('join', function(result){
+		$('#room').text(result.room);
+		$('#messages').append(divSystemConentElement('Room Changed'));
+	});
+
+	socket.on('message', function(message){
+		var newElement = $('<div></div>').text(message.text);
+		$('#messages').append(newElement);
+	});
+
+	socket.on('rooms', function(rooms){
+		$('#room-list').empty();
+
+		for(var room in rooms){
+			room = room.subString(1, room.length);
+			if(room != ''){
+				$('#room-list').append(divEscapedContentElement(room));
+			}
+		}
+
+		$(( '#room-list div').click(function() {
+			chatApp.processCommand('/join' + $(this).text());
+			$('#send-message').focus();
+		}));
+	});
+
+	setInterval(function() {
+		socket.emit('rooms');
+	}, 1000);
+
+	$('#send-message').focus();
+
+	$('#send-form').submit(function(){
+		processUserInput(chatApp, socket);
+		return false;
+	});
+});
